8a565c51a87da52e37b9e91623b790a8f60f4045
Samuel Tardieu <sam@rfc1149.net>
2011-09-02 13:50:58 +0200

    Switch to sbt 0.10.0 and android-plugin 0.6.0-SNAPSHOT

---
+++ project/build.scala
@@ -0,0 +1,36 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+
+object General {
+  val settings = Defaults.defaultSettings ++ Seq (
+    name := "Harass Me",
+    version := "1.6",
+    scalaVersion := "2.9.0-1",
+    platformName in Android := "android-11"
+  )
+
+  lazy val fullAndroidSettings =
+    General.settings ++
+    AndroidProject.androidSettings ++
+    TypedResources.settings ++
+    AndroidMarketPublish.settings ++ Seq (
+      keyalias in Android := "rfc1149",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+    )
+}
+
+object AndroidBuild extends Build {
+  lazy val main = Project (
+    "Harass Me",
+    file("."),
+    settings = General.fullAndroidSettings
+  )
+
+  lazy val tests = Project (
+    "tests",
+    file("tests"),
+    settings = General.settings ++ AndroidTest.androidSettings
+  ) dependsOn main
+}
