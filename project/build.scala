d912f179da7cc31b356ae79e65f917a33886152c
Samuel Tardieu <sam@rfc1149.net>
2012-04-09 11:23:08 +0200

    Remove unused library

--- project/build.scala
+++ project/build.scala
@@ -20,7 +20,6 @@ object General {
     AndroidMarketPublish.settings ++
     Seq (
       keyalias in Android := "rfc1149",
-      libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test",
       proguardOption in Android := "-keep public class net.rfc1149.harassme.backup.HarassMeBackupAgent -printseeds target/seeds.txt",
       proguardOptimizations in Android := Seq("-optimizations !code/simplification/arithmetic")
     )
